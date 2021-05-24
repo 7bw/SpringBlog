56019f98ca52c575f5f0cc8219efc20e160fe14d
7bw <boylee99@hotmail.com>
2021-05-23 23:17:35 -0600

    small fix

--- src/main/java/com/example/springblog/web/HomeController.java
+++ src/main/java/com/example/springblog/web/HomeController.java
@@ -4,10 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
 @Controller
 public class HomeController {
 
-    @RequestMapping(value="/")
+    @RequestMapping(value="/", method = GET)
     public String home(Model model) {
         // To do: change later
         return "home";
