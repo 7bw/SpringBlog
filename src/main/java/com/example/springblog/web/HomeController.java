f0c2ffb5179c354e98bf45cd6d389626d5e94760
7bw <boylee99@hotmail.com>
2021-05-25 22:42:08 -0600

    change route in HomeController

--- src/main/java/com/example/springblog/web/HomeController.java
+++ src/main/java/com/example/springblog/web/HomeController.java
@@ -7,9 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
-    @RequestMapping(value="/", method = GET)
+    @RequestMapping(method = GET)
     public String home(Model model) {
         // To do: change later
         return "home";
