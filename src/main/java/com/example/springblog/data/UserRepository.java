d98936fcda9c8e0b6fa880171b2576e00c4ec1db
7bw <boylee99@hotmail.com>
2021-05-29 22:59:38 -0600

    UserRepo interface

--- src/main/java/com/example/springblog/data/UserRepository.java
+++ src/main/java/com/example/springblog/data/UserRepository.java
@@ -1,6 +1,10 @@
 package com.example.springblog.data;
 
+import com.example.springblog.User;
+
 public interface UserRepository {
 
-    User save
+    User save(User user);
+
+    User findByUsername(String username);
 }
