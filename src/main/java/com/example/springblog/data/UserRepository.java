66cb55db29176bee7fe6a30c3e337b36cb73fb62
7bw <boylee99@hotmail.com>
2021-05-29 23:02:20 -0600

    fix autowire in intellij

--- src/main/java/com/example/springblog/data/UserRepository.java
+++ src/main/java/com/example/springblog/data/UserRepository.java
@@ -1,7 +1,9 @@
 package com.example.springblog.data;
 
 import com.example.springblog.User;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository {
 
     User save(User user);
