c213370f2fc915ecae3671a6a4b01e9bc784163b
7bw <boylee99@hotmail.com>
2021-05-29 23:01:51 -0600

    User controller

--- src/main/java/com/example/springblog/web/UserController.java
+++ src/main/java/com/example/springblog/web/UserController.java
@@ -1,9 +1,13 @@
 package com.example.springblog.web;
 
+import com.example.springblog.User;
+import com.example.springblog.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 @RequestMapping("/user")
@@ -11,9 +15,20 @@ public class UserController {
 
     private UserRepository userRepository;
 
+    @Autowired
+    public UserController(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
 
     @RequestMapping(value="/register", method=GET)
     public String showRegistrationForm(){
         return "registerForm";
+    }
+
+    @RequestMapping(value="/register", method=POST)
+    public String processRegistration(User user){
+        userRepository.save(user);
+        return "redirect:/user/" + user.getUsername();
     }
 }
