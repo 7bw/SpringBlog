140e4ca960fd5948370266f2ed967872cab16a46
7bw <boylee99@hotmail.com>
2021-06-02 22:49:37 -0600

    fix these later

--- src/main/java/com/example/springblog/User.java
+++ src/main/java/com/example/springblog/User.java
@@ -9,23 +9,23 @@ public class User {
     private Long id;
 
     @NotNull
-    @Size(min=5, max=16)
+    //@Size(min=5, max=16)
     private String username;
 
     @NotNull
-    @Size(min=5, max=25)
+    //@Size(min=5, max=25)
     private String password;
 
     @NotNull
-    @Size(min=2, max=30)
+    //@Size(min=2, max=30)
     private String firstName;
 
     @NotNull
-    @Size(min=2, max=30)
+    //@Size(min=2, max=30)
     private String lastName;
 
     @NotNull
-    @Email
+    //@Email
     private String email;
 
     public User() {}
