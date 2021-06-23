017a92eb760045eec2e7b9d9888e61865f21d4b9
7bw <boylee99@hotmail.com>
2021-06-22 22:17:47 -0600

    Rootconfig

--- src/main/java/com/example/springblog/config/RootConfig.java
+++ src/main/java/com/example/springblog/config/RootConfig.java
@@ -3,17 +3,25 @@ package com.example.springblog.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.core.type.filter.RegexPatternTypeFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import java.util.regex.Pattern;
 
 
 @Configuration
+@Import(DataConfig.class)
 @ComponentScan(basePackages = {"com.example.springblog"},
     excludeFilters = {
-        @ComponentScan.Filter(type= FilterType.ANNOTATION, value= EnableWebMvc.class)
+        @Filter(type= FilterType.CUSTOM, value= WebPackage.class)
     })
 public class RootConfig {
-
+	public static class WebPackage extends RegexPatternTypeFilter {
+    public WebPackage() {
+      super(Pattern.compile("springblog\\.web"));
+    }    
+  }
 
 }
