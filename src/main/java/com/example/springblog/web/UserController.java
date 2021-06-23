9632b788d10323058208666db97b7438a1081aa3
7bw <boylee99@hotmail.com>
2021-06-22 22:23:43 -0600

    modified usercontroller

--- src/main/java/com/example/springblog/web/UserController.java
+++ src/main/java/com/example/springblog/web/UserController.java
@@ -1,13 +1,17 @@
 package com.example.springblog.web;
 
+import javax.validation.Valid;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
+
 import com.example.springblog.User;
 import com.example.springblog.data.UserRepository;
+import org.springframework.validation.Errors;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 @RequestMapping("/user")
@@ -27,8 +31,20 @@ public class UserController {
     }
 
     @RequestMapping(value="/register", method=POST)
-    public String processRegistration(User user){
+    public String processRegistration(
+        @Valid User user,
+        Errors errors){
+        if (errors.hasErrors()){
+            return "registerForm";
+        }
         userRepository.save(user);
         return "redirect:/user/" + user.getUsername();
     }
+
+    @RequestMapping(value="/{username}", method=GET)
+    public String showUserProfile(@PathVariable String username, Model model) {
+        User user = userRepository.findByUsername(username);
+        model.addAttribute(user);
+        return "profile";
+      }
 }
