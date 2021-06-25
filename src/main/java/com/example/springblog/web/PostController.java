cb532a62d971937f40dbc7aa70807d5d85ab66d2
7bw <boylee99@hotmail.com>
2021-06-24 22:28:47 -0600

    modified postController

--- src/main/java/com/example/springblog/web/PostController.java
+++ src/main/java/com/example/springblog/web/PostController.java
@@ -1,5 +1,7 @@
 package com.example.springblog.web;
 
+import java.util.Date;
+import java.util.List;
 
 import com.example.springblog.data.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,10 +9,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import springblog.Post;
+import springblog.dataa.PostRepository;
 
 @Controller
 @RequestMapping("/posts")
 public class PostController {
+
+    private static final String MAX_LONG_AS_STRING = "9223372036854775807";
 
     private PostRepository postRepository;
 
@@ -20,10 +29,24 @@ public class PostController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public String posts(Model model){
-        model.addAttribute(
-                postRepository.findPosts(Long.MAX_VALUE, 20)
-        );
-        return "posts";
+    public List<Post> posts(
+        @RequestParam(value="max", defaultValue=MAX_LONG_AS_STRING) long max,
+        @RequestParam(value="count", defaultValue="20") int count){
+        return postRepository.findPosts(max, count);
+    }
+
+    @RequestMapping(value="/{postID}", method=RequestMethod.GET)
+    public String post(
+        @PathVariable("postID") long postID,
+        Model model) {
+        model.addAttribute(postRepository.findOne(postID));
+        return "post";
+    }
+    
+    @RequestMapping(method=RequestMethod.POST)
+    public String savePost(PostForm form, Model model) throws Exception {
+        postRepository.save(new Post(null, form.getMessage(), new Date(),
+            form.getLongitude(), form.getLatitude()));
+        return "redirect:/posts";
     }
 }
